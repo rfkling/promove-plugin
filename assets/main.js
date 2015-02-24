e86af4ae0d311eb09b0e63d38a8baea44b766afa
Pedro Melo <pscelzamelo@gmail.com>
2015-02-24 16:47:18 -0300

    Missing dependecy of 'app' on 'ngRoute'

--- assets/main.js
+++ assets/main.js
@@ -1,32 +1,33 @@
-﻿var baseUrl = '/redmine/plugin_assets/promove/';
+﻿
+var baseUrl = '/redmine/plugin_assets/promove/';
 
 require.config({
-    baseUrl: baseUrl.concat('app'),
-    paths: {
-        'angular': baseUrl + 'bower_components/angular/angular',
-        'angular-route': baseUrl + 'bower_components/angular-route/angular-route',
-        'ui-bootstrap': baseUrl + 'bower_components/angular-bootstrap/ui-bootstrap',
-        'underscore': baseUrl + 'bower_components/underscore/underscore',
-        'highcharts': baseUrl + 'bower_components/highcharts-release/highcharts'
+  baseUrl: baseUrl.concat('app'),
+  paths: {
+    'angular': baseUrl + 'bower_components/angular/angular',
+    'angular-route': baseUrl + 'bower_components/angular-route/angular-route',
+    'ui-bootstrap': baseUrl + 'bower_components/angular-bootstrap/ui-bootstrap',
+    'underscore': baseUrl + 'bower_components/underscore/underscore',
+    'highcharts': baseUrl + 'bower_components/highcharts-release/highcharts'
+  },
+  shim: {
+    'angular': {},
+    'angular-route': {
+      deps: ['angular']
     },
-    shim: {
-        'angular': {},
-        'angular-route': {
-            deps: ['angular']
-        },
-        'ui-bootstrap': {
-            deps: ['angular']
-        },
-        'app': {
-            deps: ['angular']
-        },
-        'underscore': {
-            exports: '_'
-        }
+    'ui-bootstrap': {
+      deps: ['angular']
     },
-    waitSeconds: 0
+    'app': {
+      deps: ['angular', 'angular-route']
+    },
+    'underscore': {
+      exports: '_'
+    }
+  },
+  waitSeconds: 0
 });
 
-require(['angular', 'angular-route', 'ui-bootstrap', 'underscore','highcharts', 'app'], function () {
-    //Application has started
+require(['angular', 'angular-route', 'ui-bootstrap', 'underscore', 'highcharts', 'app'], function() {
+  //Application has started
 });
